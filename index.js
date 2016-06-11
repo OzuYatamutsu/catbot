32f11b223b006347759a05f18f96cbd7e760e21a
Sean Collins <scollins36@gatech.edu>
2016-06-10 20:22:55 -0400

    Dumb initial proof of concept

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+const Discord = require('discord.io');
 const config = require('./config');
+
 var bot = new Discord.Client({
   token: config.token,
   autorun: true
 });
 
 bot.on('ready', _ => {
-  bot.connect()
+  bot.connect();
+  bot.setPresence({game: 'Catting around'});
   console.log(`${bot.username}(${bot.id}) is ready, myan!`);
 });
 
@@ -15,6 +18,30 @@ bot.on('message', (user, userId, channelId, message, event) => {
       bot.sendMessage({
         to: channelId,
         message: 'steak here =｀ω´='
+      });
+      break;
+    case 'good ket':
+      bot.sendMessage({
+        to: channelId,
+        message: '=´∇｀='
+      });
+      break;
+    case 'Jinhai wants a debug':
+      bot.sendMessage({
+        to: channelId,
+        message: `yiss b0ss\nid:${bot.id}\ncid:${channelId}\nws_obj:${JSON.stringify(event)}`
+      });
+      break;
+    case '!debug fire cat open-mouth':
+      bot.sendMessage({
+        to: channelId,
+        message: "https://steakscorp.org/public_utils/catcons/images/cat.png"
+     });
+     break;
+    case 'catbot you need to go to bed myan':
+      bot.sendMessage({
+        to: channelId,
+        message: 'Time for sleeps =-w-= \n http://www.ohgizmo.com/wp-content/uploads/2014/03/Cat-Burger-Pillow.jpg'
       });
       break;
     default:
