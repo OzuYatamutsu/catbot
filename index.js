0d394fad0df156a455b8d588a05fcf2a5743f25f
Sean Collins <scollins36@gatech.edu>
2016-06-10 21:16:34 -0400

    Move out handling to seperate file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.io');
 const config = require('./config');
+const handler = require('./message-handler');
 
 var bot = new Discord.Client({
   token: config.token,
@@ -7,44 +8,24 @@ var bot = new Discord.Client({
 });
 
 bot.on('ready', _ => {
-  bot.connect();
-  bot.setPresence({game: 'Catting around'});
+  connectBot();
   console.log(`${bot.username}(${bot.id}) is ready, myan!`);
 });
 
 bot.on('message', (user, userId, channelId, message, event) => {
-  switch (message) {
-    case 'fresh steak':
-      bot.sendMessage({
-        to: channelId,
-        message: 'steak here =｀ω´='
-      });
-      break;
-    case 'good ket':
-      bot.sendMessage({
-        to: channelId,
-        message: '=´∇｀='
-      });
-      break;
-    case 'Jinhai wants a debug':
-      bot.sendMessage({
-        to: channelId,
-        message: `yiss b0ss\nid:${bot.id}\ncid:${channelId}\nws_obj:${JSON.stringify(event)}`
-      });
-      break;
-    case '!debug fire cat open-mouth':
-      bot.sendMessage({
-        to: channelId,
-        message: "https://steakscorp.org/public_utils/catcons/images/cat.png"
-     });
-     break;
-    case 'catbot you need to go to bed myan':
-      bot.sendMessage({
-        to: channelId,
-        message: 'Time for sleeps =-w-= \n http://www.ohgizmo.com/wp-content/uploads/2014/03/Cat-Burger-Pillow.jpg'
-      });
-      break;
-    default:
-      break;
-  }
+  if (!handler[message]) return;
+  bot.sendMessage({
+    to: channelId,
+    message: handler[message]
+  });
 });
+
+/*
+ * Connects the bot + sets config options
+ */
+function connectBot() {
+  const nowPlaying = config.playing;
+
+  bot.connect();
+  bot.setPresence({game: nowPlaying});
+}
---
+++ message-handler.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "Fresh steak": `steak here =｀ω´=`,
+  "good ket": `=´∇｀=`,
+  "Jinhai wants a debug": `yiss b0ss, but out of function scope!! D:`,
+  "!debug fire cat open-mouth": `https://steakscorp.org/public_utils/catcons/images/cat.png`,
+  "catbot you need to go to bed myan": `Time for sleeps =-w-= \n http://www.ohgizmo.com/wp-content/uploads/2014/03/Cat-Burger-Pillow.jpg`
+};
