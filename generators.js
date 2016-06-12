3f93cbc90317c4b89e2225a58eba2c97181b56e2
Sean Collins <scollins36@gatech.edu>
2016-06-12 11:39:56 -0400

    Debug random numbers + generator funcs

--- generators.js
+++ generators.js
@@ -1,14 +1,30 @@
 const googleImages = require('google-images');
 
+function randomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+
+
 module.exports = {
-  "onDebugRunFunc": _ => { return 9+9; },
+  "rand": function(len) {
+    return Math.random();
+  },
+  "onDebugRunFunc": _ => { return Promise.resolve(9+9); },
   "findCatPic": _ => {
     const cse_id = "007659116903720282115:lxppsh-kie8";
     const api_id = "AIzaSyAxhbuxIBORj6IQv53de76fb8V-BO9IsrE";
     let client = googleImages(cse_id, api_id);
-    client.search('cat')
+    return client.search('cat')
       .then((images) => {
-        return images[0]; 
+        var index = Math.floor(Math.random() * images.length);
+        return Promise.resolve(
+          `DEBUG: I generated a ${index} with ${images.length} results\n ${images[index].url}`
+        );
       });
+  },
+  "onDebugRand": function() {
+    const len = 10;
+    return Promise.resolve(this.rand(len));
   }
 };
--- message-handler.js
+++ message-handler.js
@@ -4,17 +4,16 @@ const generators = require('./generators');
  * Returns a Promise with a static string response.
  */
 function stringRespond(str) {
-  return new Promise(() => {
-    return str;
-  });
+  return Promise.resolve(str);
 }
 
 module.exports = {
   "Fresh steak": stringRespond(`steak here =｀ω´=`),
   "good ket": stringRespond(`=´∇｀=`),
-  "Jinhai wants a debug": stringRespond(`yiss b0ss, but out of function scope!! D:`),
   "!debug fire cat open-mouth": stringRespond(`https://steakscorp.org/public_utils/catcons/images/cat.png`),
   "catbot you need to go to bed myan": stringRespond(`Time for sleeps =-w-= \n http://www.ohgizmo.com/wp-content/uploads/2014/03/Cat-Burger-Pillow.jpg`),
+  "ping": stringRespond(`pong`),
   "!debug run-func": generators.onDebugRunFunc(),
-  "!catpic": generators.findCatPic()
+  "!catpic": generators.findCatPic(),
+  "!debug rand": generators.onDebugRand()
 };
