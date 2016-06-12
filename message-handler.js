2253e73a915fc2c1af90f32caff597c37a5078df
Sean Collins <scollins36@gatech.edu>
2016-06-12 19:58:00 -0400

    Not case-sensitive

--- message-handler.js
+++ message-handler.js
@@ -9,7 +9,7 @@ function stringRespond(str) {
 
 module.exports = {
   "@catbot": _ => { return stringRespond(`_The cat completely ignores you._`) },
-  "Fresh steak": _ => { return stringRespond(`steak here =｀ω´=`) },
+  "fresh steak": _ => { return stringRespond(`steak here =｀ω´=`) },
   "good ket": _ => { return stringRespond(`=´∇｀=`) },
   "!debug fire cat open-mouth": _ => { return stringRespond(`https://steakscorp.org/public_utils/catcons/images/cat.png`) },
   "catbot you need to go to bed myan": _ => { return stringRespond(`Time for sleeps =-w-= \n http://www.ohgizmo.com/wp-content/uploads/2014/03/Cat-Burger-Pillow.jpg`) },
