348c28c7ae5d80c95bc229abe3c5300ee549ee6b
Sean Collins <scollins36@gatech.edu>
2016-06-29 22:28:11 -0400

    Finish imageidentify

--- generators.js
+++ generators.js
@@ -164,26 +164,23 @@ module.exports = {
   },
   "doImageIdentify": (args) => {
     let search = args.message.split("!catbot identify")[1].trim();
-    let uri = "https://www.imageidentify.com/objects/user-26a7681f-4b48-4f71-8f9f-93030898d70d/prd/urlapi";
+    let uri = `https://www.imageidentify.com/objects/user-26a7681f-4b48-4f71-8f9f-93030898d70d/prd/urlapi?image=${search}`;
     let options = {
-      method: "POST",
       uri,
-      body: {
-        image: search
-      },
       json: true
     };
     
     return request(options)
       .then((body) => {
-        // TODO
-        return Promise.resolve(JSON.stringify(body));
+        if (!body.identify || !body.identify.title) return Promise.resolve(`arr, dun knｏｗ　ｗｔｆ　ｉｓ　ｔｈａｔ　ｓｏｎ =｀ェ´=`);
+        return Promise.resolve(`That looks like **${body.identify.title}** to me, myan!`);
       });
   },
   "doHelp": _ => {
     return Promise.resolve(`_ａｈｈ　ｙｉｓｓ，　ｄａ　ＨＥＬＰＴＥＸＴ　ｙｏｕ　ｏｒｄｅｒ　=｀ω´=_ \n \n` 
     + "`!catbot alpha <search>` - Interprets `<search>` and gives you an answer (Wolfram|Alpha).\n\n" 
     + "`!catbot video <search>` - Finds `<search>` on YouTube.\n\n"
+    + "`!catbot identify <image_link>` - Tries to tell you what your picture is!\n\n"
     + "`!catbot react <search>` - Searches for the closest reaction called `<search>`.\n\n"
     + "**-----**\n\n"
     + "`!catbot catpic` - Returns a random cat picture.\n\n"
