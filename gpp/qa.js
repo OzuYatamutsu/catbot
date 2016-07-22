6f3989cfca2bb0cfe64be53fb2d8bab805d1fd4c
Sean Collins <scollins36@gatech.edu>
2016-07-21 23:23:25 -0400

    QA gpp

---
+++ gpp/qa.js
@@ -0,0 +1,11 @@
+const defaultPersonality = require('./default');
+
+module.exports = {
+  "name": "[stressed] Catbot",
+  "playing": [
+    "Under stress"
+  ],
+  "responses": [
+    "='~'=;;"
+  ]
+};
