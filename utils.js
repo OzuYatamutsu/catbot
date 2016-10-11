e0d735bf07fe6cd81e96bb7538a7d984295eeb2d
Sean Collins <seanc@seancotech.com>
2016-10-10 22:37:52 -0500

    String table fuzzy matching

--- utils.js
+++ utils.js
@@ -4,5 +4,13 @@
   },
   randInt: function (min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
+  },
+  findSubstrInStringTable: function (haystack, needle) {
+    for (let i = 0; i < Object.keys(haystack).length; i++) {
+      if (needle.indexOf(Object.keys(haystack)[i]) !== -1)
+        return Object.keys(haystack)[i];
+    }
+
+    return null;
   }
 }
